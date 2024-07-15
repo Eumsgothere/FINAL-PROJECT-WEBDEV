6392163f0d2db95647b89026df53b5a2e5898b27
Eumsgothere <151306362+Eumsgothere@users.noreply.github.com>
2024-07-15 13:27:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -32,60 +32,12 @@ document.addEventListener("DOMContentLoaded", () => {
         createAccountForm.classList.add("form--hidden");
     });
 
-    loginForm.addEventListener("submit", async e => {
+    loginForm.addEventListener("submit", e => {
         e.preventDefault();
 
-        const username = document.querySelector("#loginUsername").value;
-        const password = document.querySelector("#loginPassword").value;
+        // Perform your AJAX/Fetch login
 
-        try {
-            const response = await fetch('/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username, password }),
-            });
-            const data = await response.json();
-
-            if (data.success) {
-                setFormMessage(loginForm, "success", "Login successful!");
-            } else {
-                setFormMessage(loginForm, "error", "Invalid username/password combination");
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    });
-
-    createAccountForm.addEventListener("submit", async e => {
-        e.preventDefault();
-
-        const signupUsername = document.querySelector("#signupUsername").value;
-        const signupEmail = document.querySelector("#signupEmail").value;
-        const signupPassword = document.querySelector("#signupPassword").value;
-        const signupConfirmPassword = document.querySelector("#signupConfirmPassword").value;
-
-        try {
-            const response = await fetch('/signup', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ signupUsername, signupEmail, signupPassword, signupConfirmPassword }),
-            });
-            const data = await response.json();
-
-            if (data.success) {
-                setFormMessage(createAccountForm, "success", "Account created successfully. Please log in.");
-                createAccountForm.classList.add("form--hidden");
-                loginForm.classList.remove("form--hidden");
-            } else {
-                setFormMessage(createAccountForm, "error", data.message);
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
+        setFormMessage(loginForm, "error", "Invalid username/password combination");
     });
 
     document.querySelectorAll(".form__input").forEach(inputElement => {
