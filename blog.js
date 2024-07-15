eb017c7b9ad9b9f086e39a5bb3a123fa5a3148c4
Eumsgothere <151306362+Eumsgothere@users.noreply.github.com>
2024-07-15 13:21:18 +0800

    Add files via upload

--- blog.js
+++ blog.js
@@ -1,23 +1,19 @@
-// blog.js
 
-// Get the new blog post form
 const newBlogPostForm = document.getElementById('new-blog-post-form');
 
-// Add an event listener to the form submission
 newBlogPostForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  // Get the form data
+
   const title = document.getElementById('title').value;
   const content = document.getElementById('content').value;
 
-  // Create a new blog post object
+
   const blogPost = {
     title: title,
     content: content
   };
 
-  // Send the blog post data to the server
   fetch('/api/blog-posts', {
     method: 'POST',
     headers: {
@@ -28,7 +24,7 @@ newBlogPostForm.addEventListener('submit', (e) => {
   .then(response => response.json())
   .then(data => {
     console.log(data);
-    // Redirect to the blog list page
+ 
     window.location.href = '/blog';
   })
   .catch(error => {
@@ -36,20 +32,19 @@ newBlogPostForm.addEventListener('submit', (e) => {
   });
 });
 
-// Get the blog post editor
+
 const blogPostEditor = document.getElementById('blog-post-editor');
 
-// Add an event listener to the save button
+
 blogPostEditor.addEventListener('click', (e) => {
   e.preventDefault();
 
-  // Get the blog post ID
+
   const blogPostId = blogPostEditor.dataset.blogPostId;
 
-  // Get the updated blog post content
+
   const content = document.getElementById('editor').value;
 
-  // Send the updated blog post data to the server
   fetch(`/api/blog-posts/${blogPostId}`, {
     method: 'PATCH',
     headers: {
@@ -60,7 +55,6 @@ blogPostEditor.addEventListener('click', (e) => {
   .then(response => response.json())
   .then(data => {
     console.log(data);
-    // Redirect to the blog list page
     window.location.href = '/blog';
   })
   .catch(error => {
